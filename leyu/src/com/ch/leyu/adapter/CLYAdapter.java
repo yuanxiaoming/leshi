77908b73071125850ddff0bf89e2f86a603b95c7
yuanxiaoming <xiaoming_android@163.com>
2014-04-28 01:00:33 +0800

    init ,gitgnore

--- leyu/src/com/ch/leyu/adapter/CLYAdapter.java
+++ leyu/src/com/ch/leyu/adapter/CLYAdapter.java
@@ -18,39 +18,39 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class CLYAdapter extends ArrayAdapter<LoginResponse> {
 
-	public CLYAdapter(Context context, List<LoginResponse> objects) {
-		super(context, 0, objects);
-	}
+    public CLYAdapter(Context context, List<LoginResponse> objects) {
+        super(context, 0, objects);
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder = null;
-		if (convertView == null) {
-			convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_login, parent, false);
-			holder = new ViewHolder();
-			holder.cover = (ImageView) convertView.findViewById(R.id.img_login_cover);
-			holder.area = (TextView) convertView.findViewById(R.id.txt_login_area);
-			holder.score = (TextView) convertView.findViewById(R.id.txt_login_score);
-			holder.total = (TextView) convertView.findViewById(R.id.txt_login_totalnumber);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder) convertView.getTag();
-		}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_login, parent, false);
+            holder = new ViewHolder();
+            holder.cover = (ImageView) convertView.findViewById(R.id.img_login_cover);
+            holder.area = (TextView) convertView.findViewById(R.id.txt_login_area);
+            holder.score = (TextView) convertView.findViewById(R.id.txt_login_score);
+            holder.total = (TextView) convertView.findViewById(R.id.txt_login_totalnumber);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
-		LoginResponse item = getItem(position);
-		if (item != null) {
-			holder.area.setText(item.getArea());
-			holder.score.setText(item.getScore());
-			holder.total.setText(item.getCur_num());
+        LoginResponse item = getItem(position);
+        if (item != null) {
+            holder.area.setText(item.getArea());
+            holder.score.setText(item.getScore());
+            holder.total.setText(item.getCur_num());
 
-			ImageLoader.getInstance().displayImage(item.getCover(), holder.cover, ImageLoaderUtil.getImageLoaderOptions());
-		}
-		return convertView;
-	}
+            ImageLoader.getInstance().displayImage(item.getCover(), holder.cover, ImageLoaderUtil.getImageLoaderOptions());
+        }
+        return convertView;
+    }
 
-	private final class ViewHolder {
-		public ImageView cover;
-		public TextView total, score, area;
-	}
+    private final class ViewHolder {
+        public ImageView cover;
+        public TextView total, score, area;
+    }
 
 }
