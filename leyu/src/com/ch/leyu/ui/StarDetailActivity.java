ac1bd9dda4a91f713de8659a6221c2a2b2db6709
Liu Ping <275575709@qq.com>
2014-05-14 16:41:36 +0800

    明星视频修正

--- leyu/src/com/ch/leyu/ui/StarDetailActivity.java
+++ leyu/src/com/ch/leyu/ui/StarDetailActivity.java
@@ -20,6 +20,7 @@ import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
