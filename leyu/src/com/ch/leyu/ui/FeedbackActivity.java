e0337ac7018297c7443c587443844799e651dc22
Liu Ping <275575709@qq.com>
2014-05-15 17:33:55 +0800

    反馈界面

---
+++ leyu/src/com/ch/leyu/ui/FeedbackActivity.java
@@ -0,0 +1,45 @@
+
+package com.ch.leyu.ui;
+
+import com.ch.leyu.R;
+
+import android.view.View;
+
+/***
+ * 反馈
+ * 
+ * @author L
+ */
+public class FeedbackActivity extends BaseActivity {
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    protected void getExtraParams() {
+
+    }
+
+    @Override
+    protected void loadViewLayout() {
+        setContentView(R.layout.activity_feedback);
+    }
+
+    @Override
+    protected void findViewById() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void processLogic() {
+
+    }
+
+}
