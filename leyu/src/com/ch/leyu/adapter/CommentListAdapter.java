16b4ab5c41fc789b81835d6cfbe26a6b69da0a18
yuanxiaoming <xiaoming_android@163.com>
2014-06-09 22:33:45 +0800

    更新

--- leyu/src/com/ch/leyu/adapter/CommentListAdapter.java
+++ leyu/src/com/ch/leyu/adapter/CommentListAdapter.java
@@ -2,8 +2,8 @@
 package com.ch.leyu.adapter;
 
 import com.ch.leyu.R;
-import com.ch.leyu.http.utils.LeUtils;
 import com.ch.leyu.responseparse.CommentDetail;
+import com.ch.leyu.utils.LeUtils;
 
 import android.content.Context;
 import android.graphics.Color;
@@ -23,103 +23,113 @@ import java.util.ArrayList;
 
 public class CommentListAdapter extends BaseAdapter {
 
-    private ArrayList<CommentDetail> mArrayList;
+	private ArrayList<CommentDetail> mArrayList;
 
-    private LayoutInflater mInflater;
+	private LayoutInflater mInflater;
 
-    private Context mContext;
+	private Context mContext;
 
-    public CommentListAdapter(ArrayList<CommentDetail> arrayList, Context context) {
-        this.mArrayList = arrayList;
-        mInflater = LayoutInflater.from(context);
-        mContext = context;
-    }
+	public CommentListAdapter(ArrayList<CommentDetail> arrayList, Context context) {
+		if(arrayList==null){
+			this.mArrayList = new ArrayList<CommentDetail>();
+		}else{
+			this.mArrayList = arrayList;
+		}
+		mInflater = LayoutInflater.from(context);
+		mContext = context;
+	}
 
-    public void chargeArrayList(ArrayList<CommentDetail> arrayList) {
-        this.mArrayList = arrayList;
-        notifyDataSetChanged();
-    }
+	public void chargeArrayList(ArrayList<CommentDetail> arrayList) {
+		this.mArrayList = arrayList;
+		notifyDataSetChanged();
+	}
 
-    public void addArrayList(ArrayList<CommentDetail> arrayList) {
-        if (mArrayList != null) {
-            this.mArrayList.addAll(arrayList);
-            notifyDataSetChanged();
-        } else {
-            chargeArrayList(arrayList);
-        }
-    }
+	public void addArrayList(ArrayList<CommentDetail> arrayList) {
+		if (mArrayList != null) {
+			this.mArrayList.addAll(arrayList);
+			notifyDataSetChanged();
+		} else {
+			chargeArrayList(arrayList);
+		}
+	}
 
-    @Override
-    public int getCount() {
-        if (mArrayList != null) {
-            return mArrayList.size();
-        }
-        return 0;
-    }
+	public ArrayList<CommentDetail>  getArrayList() {
+		return this.mArrayList;
+	}
 
-    @Override
-    public Object getItem(int position) {
-        if (mArrayList != null) {
-            return mArrayList.get(position);
-        }
-        return null;
-    }
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+	@Override
+	public int getCount() {
+		if (mArrayList != null) {
+			return mArrayList.size();
+		}
+		return 0;
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-        if (convertView == null) {
-            holder = new ViewHolder();
-            convertView = mInflater.inflate(R.layout.comment_list_item, null);
-            holder.name = (TextView) convertView.findViewById(R.id.comment_list_tv_name);
-            holder.detail = (WebView) convertView.findViewById(R.id.comment_list_web_detail);
-            holder.time = (TextView) convertView.findViewById(R.id.comment_list_tv_time);
-            holder.reply = (TextView) convertView.findViewById(R.id.comment_list_tv_reply);
+	@Override
+	public Object getItem(int position) {
+		if (mArrayList != null) {
+			return mArrayList.get(position);
+		}
+		return null;
+	}
 
-            convertView.setTag(holder);
-        } else {
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 
-            holder = (ViewHolder) convertView.getTag();
-        }
-        holder.name.setText(mArrayList.get(position).getNickname());
-        holder.time.setText(LeUtils.toDate(mArrayList.get(position).getCreateTime()));
-        WebSettings settings = holder.detail.getSettings();
-        // 设置布局算法
-        settings.setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
-        // 设置渲染等级
-        settings.setRenderPriority(RenderPriority.HIGH);
-        // 设置缓存模式
-        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-        // 设置预览模式
-        settings.setLoadWithOverviewMode(true);
-        // 设置字体缩放级别
-        settings.setTextSize(TextSize.NORMAL);
-        // 设置支持js
-        settings.setJavaScriptEnabled(true);
-        // 设置处理客户端
-        holder.detail.setWebViewClient(new WebViewClient());
-        holder.detail.setBackgroundColor(Color.parseColor("#F0F0F0"));
-        
-        holder.detail.loadDataWithBaseURL("", mArrayList.get(position).getComment(), "text/html",
-                "UTF-8", "");
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) {
+			holder = new ViewHolder();
+			convertView = mInflater.inflate(R.layout.comment_list_item, null);
+			holder.name = (TextView) convertView.findViewById(R.id.comment_list_tv_name);
+			holder.detail = (WebView) convertView.findViewById(R.id.comment_list_web_detail);
+			holder.time = (TextView) convertView.findViewById(R.id.comment_list_tv_time);
+			holder.reply = (TextView) convertView.findViewById(R.id.comment_list_tv_reply);
 
-        return convertView;
-    }
+			convertView.setTag(holder);
+		} else {
 
-    class ViewHolder {
-        TextView name;
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.name.setText(mArrayList.get(position).getNickname());
 
-        WebView detail;
+		holder.time.setText(LeUtils.toDate(mArrayList.get(position).getCreateTime()));
+		WebSettings settings = holder.detail.getSettings();
+		// 设置布局算法
+		settings.setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
+		// 设置渲染等级
+		settings.setRenderPriority(RenderPriority.HIGH);
+		// 设置缓存模式
+		settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+		// 设置预览模式
+		settings.setLoadWithOverviewMode(true);
+		// 设置字体缩放级别
+		settings.setTextSize(TextSize.NORMAL);
+		// 设置支持js
+		settings.setJavaScriptEnabled(true);
+		// 设置处理客户端
+		holder.detail.setWebViewClient(new WebViewClient());
+		holder.detail.setBackgroundColor(Color.parseColor("#F0F0F0"));
 
-        TextView time;
+		holder.detail.loadDataWithBaseURL("", mArrayList.get(position).getComment(), "text/html",
+				"UTF-8", "");
 
-        TextView reply;
+		return convertView;
+	}
 
-    }
+	class ViewHolder {
+		TextView name;
+
+		WebView detail;
+
+		TextView time;
+
+		TextView reply;
+
+	}
 
 }
--- leyu/src/com/ch/leyu/ui/CommentFragment.java
+++ leyu/src/com/ch/leyu/ui/CommentFragment.java
@@ -11,23 +11,25 @@ import com.ch.leyu.responseparse.CommentResponse;
 import com.ch.leyu.utils.Constant;
 import com.ch.leyu.widget.view.ClearEditText;
 import com.ch.leyu.widget.xlistview.XListView;
+
 import org.apache.http.Header;
+
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 /**
  * 视频播放--评论界面
- * 
+ *
  * @author L
  */
-public class CommentFragment extends BaseFragment implements OnClickListener {
+public class CommentFragment extends BaseFragment {
 
     /** 用户id.暂时为0 */
     public static final String UID = "uid";
@@ -48,11 +50,9 @@ public class CommentFragment extends BaseFragment implements OnClickListener {
 
     private CommentListAdapter mAdapter;
 
-    private String mId;
+    private String mCid;
 
-    private String nickName = "";
-
-    private String comment = "";
+    private String nickName="";
 
     /** ListView 要加入的头部View */
     private View mListViewHeaderView;
@@ -63,24 +63,20 @@ public class CommentFragment extends BaseFragment implements OnClickListener {
     /** 有评论时回复内容 */
     private ClearEditText mDetail;
 
-    /** 小贴士 */
-    private RelativeLayout mTipsLayout;
-
     /** 无评论时发表回复 */
-    private Button mPublish_null;
+    private Button mPublish_EmptyView;
 
     /** 无评论时回复内容 */
-    private ClearEditText mDetail_null;
+    private ClearEditText mDetail_EmptyView;
 
-    private CommentDetail mCommentDetail;
-
-    private ArrayList<CommentDetail> mDetailsList;
+    /** 小贴士 */
+    private View mTipsLayout;
 
     @Override
     protected void getExtraParams() {
         Bundle bundle = getArguments();
         if (bundle != null) {
-            mId = bundle.getString(Constant.GMAE_ID);
+            mCid = bundle.getString(Constant.GMAE_ID);
             nickName = bundle.getString(Constant.NICKNAME);
         }
     }
@@ -89,6 +85,7 @@ public class CommentFragment extends BaseFragment implements OnClickListener {
     protected void loadViewLayout() {
         setContentView(R.layout.fragment_comment);
         mListViewHeaderView = LayoutInflater.from(getActivity()).inflate(R.layout.comment_head,null);
+
     }
 
     @Override
@@ -96,157 +93,131 @@ public class CommentFragment extends BaseFragment implements OnClickListener {
         mListView = (XListView) findViewById(R.id.fragment_comment_xistview);
         mPublish = (Button) mListViewHeaderView.findViewById(R.id.comment_head_bt_commit);
         mDetail = (ClearEditText) mListViewHeaderView.findViewById(R.id.comment_head_et_detail);
-        mTipsLayout = (RelativeLayout) findViewById(R.id.fragment_comment_tips);
-        mDetail_null = (ClearEditText)findViewById(R.id.comment_et_detail);
-        mPublish_null = (Button)findViewById(R.id.comment_bt_commit);
+        mPublish_EmptyView = (Button)findViewById(R.id.comment_head_bt_commit);
+        mDetail_EmptyView = (ClearEditText)findViewById(R.id.comment_head_et_detail);
+        mTipsLayout = findViewById(R.id.comment_emptyview_layout);
+        mListView.setEmptyView(mTipsLayout);
+        mListView.addHeaderView(mListViewHeaderView);
         mAdapter = new CommentListAdapter(null, getActivity());
+        mListView.setAdapter(mAdapter);
     }
 
     @Override
     protected void setListener() {
-        mPublish.setOnClickListener(this);
-        mPublish_null.setOnClickListener(this);
+        mPublish.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                hidden();
+                if (!TextUtils.isEmpty(mDetail.getText().toString())) {
+                    String  comment = mDetail.getText().toString();
+                    CommentDetail  commentDetail = new CommentDetail();
+                    commentDetail.setUid(mCid);
+                    commentDetail.setComment(comment);
+                    commentDetail.setCreateTime(System.currentTimeMillis());
+                    commentDetail.setNickname(nickName);
+                    publishComment(0,commentDetail);
+                    mDetail.setText("");
+                }else {
+                    Toast.makeText(getActivity(), R.string.comment_toast_tips, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
+        mPublish_EmptyView.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                hidden();
+                if (!TextUtils.isEmpty(mDetail_EmptyView.getText().toString())) {
+                    String  comment = mDetail_EmptyView.getText().toString();
+                    CommentDetail  commentDetail = new CommentDetail();
+                    commentDetail.setUid(mCid);
+                    commentDetail.setComment(comment);
+                    commentDetail.setCreateTime(System.currentTimeMillis());
+                    commentDetail.setNickname(nickName);
+                    publishComment(0,commentDetail);
+                    mDetail_EmptyView.setText("");
+                }else {
+                    Toast.makeText(getActivity(), R.string.comment_toast_tips, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 
     @Override
     protected void processLogic() {
-        mListView.setAdapter(mAdapter);
-        mListView.addHeaderView(mListViewHeaderView);
         RequestParams params = new RequestParams();
-        params.put("cid", mId);
+        params.put("cid", mCid);
         JHttpClient.get(getActivity(), Constant.COMMENT_LIST, params, CommentResponse.class,new DataCallback<CommentResponse>() {
 
-                    @Override
-                    public void onStart() {
-                        mHttpLoadingView.setVisibility(View.VISIBLE);
-                    }
+            @Override
+            public void onStart() {
+                mHttpLoadingView.setVisibility(View.VISIBLE);
+            }
 
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, CommentResponse data) {
-                        if (data != null) {
-                            if(mDetailsList!=null){
-                                mDetailsList.clear();
-                            }
-                            mDetailsList = data.getComment();
-                            mTipsLayout.setVisibility(View.GONE);
-                            mListView.setVisibility(View.VISIBLE);
-                            mAdapter.addArrayList(mDetailsList);
-                        } else {
-                            mListView.setVisibility(View.GONE);
-                            mTipsLayout.setVisibility(View.VISIBLE);
-                        }
-                    }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, CommentResponse data) {
+                if (data != null) {
+                    mAdapter.addArrayList(data.getComment());
+                }
+            }
 
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String responseString,
-                            Exception exception) {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString,
+                    Exception exception) {
 
-                    }
+            }
 
-                    @Override
-                    public void onFinish() {
-                        mHttpLoadingView.setVisibility(View.GONE);
-                    }
-                });
+            @Override
+            public void onFinish() {
+                mHttpLoadingView.setVisibility(View.GONE);
+            }
+        });
     }
 
     /**
-     * @param uid 用户id
-     * @param nickname 视频名字
-     * @param cid 视频id
-     * @param comment 评论内容
-     * @param index 1代表无评论时，2代表有评论时
-     */
-    private void publishComment(int uid, String nickname, String cid, String comment,final int index) {
+    * @param uid 用户id
+    * @param nickname 视频名字
+    * @param cid 视频id
+    * @param comment 评论内容
+    * @param index 1代表无评论时，2代表有评论时
+    */
+    private void publishComment(int uid, final CommentDetail  commentDetail) {
         RequestParams params = new RequestParams();
         params.put(UID, uid);
-        params.put(NICKNAME, nickname);
+        params.put(NICKNAME, commentDetail.getNickname());
         params.put(TYPE, 2);
-        params.put(CID, cid);
-        params.put(COMMENT, comment);
-        JHttpClient.post(getActivity(), Constant.COMMENT_PUBLISH, params, CommentResponse.class,
-                new DataCallback<CommentResponse>() {
+        params.put(CID, mCid);
+        params.put(COMMENT, commentDetail.getComment());
+        JHttpClient.post(getActivity(), Constant.COMMENT_PUBLISH, params, CommentResponse.class,new DataCallback<CommentResponse>() {
 
-                    @Override
-                    public void onStart() {
-                        mHttpLoadingView.setVisibility(View.VISIBLE);
-                    }
+            @Override
+            public void onStart() {
+                mHttpLoadingView.setVisibility(View.VISIBLE);
+            }
 
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, CommentResponse data) {
-                        if(index == 1)
-                        {
-                            mDetail_null.setText("");
-                            mDetailsList = new ArrayList<CommentDetail>();
-                            mDetailsList.add(mCommentDetail);
-                            mAdapter.chargeArrayList(mDetailsList);
-                            mTipsLayout.setVisibility(View.GONE);
-                            mListView.setVisibility(View.VISIBLE);
-                        }
-                        if(index == 2)
-                        {
-                            mDetail.setText("");
-                            mDetailsList.add(mCommentDetail);
-                            mAdapter.chargeArrayList(mDetailsList);
-                            mTipsLayout.setVisibility(View.GONE);
-                            mListView.setVisibility(View.VISIBLE);
-                        }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, CommentResponse data) {
+                ArrayList<CommentDetail> mDetailsList = new ArrayList<CommentDetail>();
+                mDetailsList.add(commentDetail);
+                mAdapter.addArrayList(mDetailsList);
+                Toast.makeText(getActivity(), "评论成功", Toast.LENGTH_SHORT).show();
+            }
 
-                        Toast.makeText(getActivity(), "评论成功", Toast.LENGTH_SHORT).show();
-                    }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Exception exception) {
+                Toast.makeText(getActivity(), "评论失败", Toast.LENGTH_SHORT).show();
+            }
 
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String responseString, Exception exception) {
-                        Toast.makeText(getActivity(), "评论失败", Toast.LENGTH_SHORT).show();
-                    }
+            @Override
+            public void onFinish() {
+                mHttpLoadingView.setVisibility(View.GONE);
+            }
 
-                    @Override
-                    public void onFinish() {
-                        mHttpLoadingView.setVisibility(View.GONE);
-                    }
-
-                });
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        hidden();
-        switch (v.getId()) {
-            case R.id.comment_bt_commit:
-
-                if (!mDetail_null.getText().toString().equals("")) {
-                    comment = mDetail_null.getText().toString();
-                } else {
-                    Toast.makeText(getActivity(), R.string.comment_toast_tips, Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                mCommentDetail = new CommentDetail();
-                mCommentDetail.setUid(mId);
-                mCommentDetail.setComment(comment);
-                mCommentDetail.setCreateTime(System.currentTimeMillis());
-                mCommentDetail.setNickname(nickName);
-                publishComment(0, nickName, mId, comment,1);
-                break;
-                
-            case R.id.comment_head_bt_commit:
-
-                if (!mDetail.getText().toString().equals("")) {
-                    comment = mDetail.getText().toString();
-                } else {
-                    Toast.makeText(getActivity(), R.string.comment_toast_tips, Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                mCommentDetail = new CommentDetail();
-                mCommentDetail.setUid(mId);
-                mCommentDetail.setComment(comment);
-                mCommentDetail.setCreateTime(System.currentTimeMillis());
-                mCommentDetail.setNickname(nickName);
-                publishComment(0, nickName, mId, comment,2);
-                break;
-
-            default:
-                break;
-        }
-
-    }
 }
