f2d5d8cbd57c5bc8a8ed82c8ff3eeab016418b69
Liu Ping <275575709@qq.com>
2014-06-12 18:04:07 +0800

    首页按钮点击效果

---
+++ leyu/src/com/ch/leyu/adapter/HSNewsDetailListAdapter.java
@@ -0,0 +1,106 @@
+
+package com.ch.leyu.adapter;
+
+import com.ch.leyu.R;
+import com.ch.leyu.responseparse.Property;
+import com.ch.leyu.utils.CommonUtil;
+import com.ch.leyu.utils.ImageLoaderUtil;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ImageView.ScaleType;
+
+import java.util.ArrayList;
+
+public class HSNewsDetailListAdapter extends BaseAdapter {
+
+    private ArrayList<Property> mArrayList;;
+
+    private Context mContext;
+
+    public HSNewsDetailListAdapter(Context context, ArrayList<Property> objects) {
+        this.mArrayList = objects;
+        this.mContext = context;
+    }
+
+    public void chargeArrayList(ArrayList<Property> arrayList) {
+        this.mArrayList = arrayList;
+        notifyDataSetChanged();
+    }
+
+    public void addArrayList(ArrayList<Property> arrayList) {
+        if (mArrayList != null) {
+            this.mArrayList.addAll(arrayList);
+            notifyDataSetChanged();
+        } else {
+            chargeArrayList(arrayList);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        if (mArrayList != null) {
+            return mArrayList.size();
+        }
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (mArrayList != null) {
+            return mArrayList.get(position);
+        }
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.fragment_allnew_item,
+                    parent, false);
+            holder = new ViewHolder();
+            holder.img = (ImageView) convertView.findViewById(R.id.hsnews_detail_item_img);
+            holder.title = (TextView) convertView.findViewById(R.id.hsnews_detail_item_title);
+            holder.time = (TextView) convertView.findViewById(R.id.hsnews_detail_item_time);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Property item = (Property) getItem(position);
+        if (item != null) {
+            holder.title.setText(item.getTitle());
+            holder.time.setText(item.getCreateTime());
+            holder.img.setLayoutParams(new RelativeLayout.LayoutParams(CommonUtil.getWidthMetrics(mContext) / 4, CommonUtil.getWidthMetrics(mContext) / 5));
+            holder.img.setScaleType(ScaleType.FIT_XY) ;
+            if (item.getImageSrc() == null || item.getImageSrc().equals("")) {
+                holder.img.setVisibility(View.GONE);
+
+            }else {
+                ImageLoader.getInstance().displayImage(item.getImageSrc(), holder.img , ImageLoaderUtil.getImageLoaderOptions());
+            }
+
+        }
+        return convertView;
+    }
+
+    private final class ViewHolder {
+        public ImageView img;
+
+        public TextView title, time;
+    }
+
+}
---
+++ leyu/src/com/ch/leyu/ui/HSNewsDetailFragment.java
@@ -0,0 +1,165 @@
+
+package com.ch.leyu.ui;
+
+import com.ch.leyu.R;
+import com.ch.leyu.adapter.CLYAdapter;
+import com.ch.leyu.adapter.HSNewsDetailListAdapter;
+import com.ch.leyu.http.httplibrary.RequestParams;
+import com.ch.leyu.http.work.DataCallback;
+import com.ch.leyu.http.work.JHttpClient;
+import com.ch.leyu.responseparse.AllNewResponse;
+import com.ch.leyu.responseparse.Property;
+import com.ch.leyu.utils.Constant;
+import com.ch.leyu.widget.xlistview.XListView;
+import com.ch.leyu.widget.xlistview.XListView.IXListViewListener;
+
+import org.apache.http.Header;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/***
+ * 炉石传说新闻内页
+ * 
+ * @author Administrator
+ */
+public class HSNewsDetailFragment extends BaseFragment implements OnItemClickListener {
+    private XListView mXListView;
+
+    private HSNewsDetailListAdapter mAdapter;
+
+    private int mPage = 1;
+
+    private int mTotalPage;
+
+    private SimpleDateFormat mSimpleDateFormat;
+    
+    private String mCid ;
+
+    @Override
+    protected void getExtraParams() {
+
+    }
+
+    @Override
+    protected void loadViewLayout() {
+        setContentView(R.layout.fragment_hsnews_detail);
+    }
+
+    @Override
+    protected void findViewById() {
+        mXListView = (XListView) findViewById(R.id.hsnews_detail_listview_cly);
+        mAdapter = new HSNewsDetailListAdapter(getActivity(), null);
+    }
+
+    @Override
+    protected void setListener() {
+        mXListView.setPullRefreshEnable(true);
+        mXListView.setPullLoadEnable(true);
+        mXListView.setOnItemClickListener(this);
+        mXListView.setXListViewListener(mIXListViewListenerImp);
+    }
+
+    @Override
+    protected void processLogic() {
+        mXListView.setAdapter(mAdapter);
+        requestData(mPage);
+    }
+    
+    private void requestData(int page) {
+        RequestParams params = new RequestParams();
+        params.put(Constant.GMAE_ID, "23");
+        params.put(Constant.CID, mCid);
+        params.put(Constant.PAGE, page);
+        JHttpClient.get(getActivity(), Constant.URL+Constant.ALL_NEWS+Constant.RESTS_NEWS, params, AllNewResponse.class,new DataCallback<AllNewResponse>() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, AllNewResponse data) {
+              if(data!=null){
+                  mTotalPage = data.getTotalPage();
+                  if (mPage == 1) {
+                      mAdapter.chargeArrayList(data.getNewsList());
+                  } else {
+                      mAdapter.addArrayList(data.getNewsList());
+                  }
+                  
+                  mPage++;
+                  if (mPage > mTotalPage) {
+                      mXListView.setPullLoadEnable(false);
+                  } else {
+                      mXListView.setPullLoadEnable(true);
+                  }
+              }
+            }
+
+            @Override
+            public void onStart() {
+                if(mPage==1){
+                    mHttpLoadingView.setVisibility(View.VISIBLE);
+                }
+                if (mXListView != null) {
+                    onLoad();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                mHttpLoadingView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString,Exception exception) {
+           
+            }
+        });
+        
+    }
+    
+    
+    /**
+     * 上拉加载，下拉刷新
+     */
+    private XListView.IXListViewListener mIXListViewListenerImp = new IXListViewListener() {
+        @Override
+        public void onRefresh() {
+            mPage = 1;
+            requestData(mPage);
+        }
+
+        @Override
+        public void onLoadMore() {
+            requestData(mPage);
+
+        }
+    };
+
+    /**
+     * 时间监听
+     */
+    @SuppressLint("SimpleDateFormat")
+    private void onLoad() {
+        mXListView.stopRefresh();
+        mXListView.stopLoadMore();
+        mSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        mXListView.setRefreshTime(mSimpleDateFormat.format(new Date()));
+    }
+    
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Property item = (Property) parent.getAdapter().getItem(position);
+        if(item!=null){
+            Intent intent = new Intent(getActivity(), NewsDetailActivity.class);
+            intent.putExtra(Constant.CID, item.getId());
+            startActivity(intent);
+        }
+        
+    }
+
+}
